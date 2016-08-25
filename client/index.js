fd07bc556e8bde1023fc92068807cd0b1bcb697d
lschpmn <luis@lmc-productions.com>
2016-08-25 16:05:55 -0500

    Added material-ui

--- client/index.js
+++ client/index.js
@@ -1,8 +1,16 @@
 'use strict';
 
 import './index.html';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React from 'react';
 import ReactDom from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import Root from './components/Root';
 
-ReactDom.render(<Root/>, document.getElementById('main'));
+injectTapEventPlugin();
+
+const App = () => <MuiThemeProvider>
+  <Root />
+</MuiThemeProvider>;
+
+ReactDom.render(<App />, document.getElementById('main'));
