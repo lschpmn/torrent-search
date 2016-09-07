cc1f550cb3cd4f463f540ab83ddcb965da6b51fe
lschpmn <luis@lmc-productions.com>
2016-09-06 21:36:25 -0500

    Every column can now be sorted in both directions by clicking on the header, except for name column because that seems unnecessary

--- client/components/Body.js
+++ client/components/Body.js
@@ -8,6 +8,18 @@ import {connect} from 'react-redux';
 
 class Body extends Component {
   /**
+   * @param {torrentResult[]} _props.results
+   */
+  constructor(_props) {
+    super(_props);
+    this.props = _props;
+    this.state = {
+      sortProp: 'seed',
+      sortDirection: false //false is descending, true is ascending
+    };
+  }
+  
+  /**
    * @param {Number} bytes
    * @private
    */
@@ -34,24 +46,39 @@ class Body extends Component {
     return moment(timestamp).fromNow();
   }
   
+  /**
+   * @param {String} sortProp - The property on the torrentResult object that will be sorted
+   * @private
+   */
+  _sort(sortProp) {
+    if(this.state.sortProp === sortProp) this.setState({sortDirection: !this.state.sortDirection});
+    else this.setState({sortDirection: false, sortProp});
+  }
+  
   render() {
-    console.log(this.props.results);
-    const rows = this.props.results.map(/**torrentResult*/ result => {
-      return <TableRow 
-        key={result.magnetLink} 
-        hoverable={true}
-        selectable={false}
-      >
-        <TableRowColumn style={styles.tableName}>
-          {result.name}
-          <a href={result.magnetLink}><FileCloudDownload style={styles.icon}/></a>
-        </TableRowColumn>
-        <TableRowColumn style={styles.tableDateSize}>{this._convertTimestamp(result.date)}</TableRowColumn>
-        <TableRowColumn style={styles.tableDateSize}>{this._convertBytes(result.size)}</TableRowColumn>
-        <TableRowColumn style={styles.tableLeechSeed}>{result.seed}</TableRowColumn>
-        <TableRowColumn style={styles.tableLeechSeed}>{result.leech}</TableRowColumn>
-      </TableRow>;
-    });
+    console.log(this.state);
+    const rows = this.props.results
+      .sort((resultA, resultB) => {
+        return this.state.sortDirection 
+          ? resultA[this.state.sortProp] - resultB[this.state.sortProp]
+          : resultB[this.state.sortProp] - resultA[this.state.sortProp];
+      })
+      .map(/**torrentResult*/ result => {
+        return <TableRow
+          key={result.magnetLink}
+          hoverable={true}
+          selectable={false}
+        >
+          <TableRowColumn style={styles.tableName}>
+            {result.name}
+            <a href={result.magnetLink}><FileCloudDownload style={styles.icon}/></a>
+          </TableRowColumn>
+          <TableRowColumn style={styles.tableDateSize}>{this._convertTimestamp(result.date)}</TableRowColumn>
+          <TableRowColumn style={styles.tableDateSize}>{this._convertBytes(result.size)}</TableRowColumn>
+          <TableRowColumn style={styles.tableLeechSeed}>{result.seed}</TableRowColumn>
+          <TableRowColumn style={styles.tableLeechSeed}>{result.leech}</TableRowColumn>
+        </TableRow>;
+      });
   
     return <div style={styles.container}>
       <h1 style={styles.title}>Search Results</h1>
@@ -60,10 +87,14 @@ class Body extends Component {
         <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
           <TableRow>
             <TableHeaderColumn style={styles.tableName}>Name</TableHeaderColumn>
-            <TableHeaderColumn style={styles.tableDateSize}>Age</TableHeaderColumn>
-            <TableHeaderColumn style={styles.tableDateSize}>Size</TableHeaderColumn>
-            <TableHeaderColumn style={styles.tableLeechSeed}>Seeds</TableHeaderColumn>
-            <TableHeaderColumn style={styles.tableLeechSeed}>Leeches</TableHeaderColumn>
+            
+            <TableHeaderColumn style={styles.tableDateSize}><div onClick={() => this._sort('date')}>Age</div></TableHeaderColumn>
+            
+            <TableHeaderColumn style={styles.tableDateSize}><div onClick={() => this._sort('size')}>Size</div></TableHeaderColumn>
+            
+            <TableHeaderColumn style={styles.tableLeechSeed}><div onClick={() => this._sort('seed')}>Seeds</div></TableHeaderColumn>
+            
+            <TableHeaderColumn style={styles.tableLeechSeed}><div onClick={() => this._sort('leech')}>Leeches</div></TableHeaderColumn>
           </TableRow>
         </TableHeader>
       
