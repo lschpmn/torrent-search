7888957b501c62b73b230f1832d7168dd80e0c2d
lschpmn <luis@lmc-productions.com>
2017-11-24 18:10:50 -0600

    Better TorrentFinder functions

--- server/TorrentFinder/index.js
+++ server/TorrentFinder/index.js
@@ -17,16 +17,20 @@ class TorrentFinder {
   static MergeResults(oldResults, newResults) {
     return [...oldResults, ...newResults]
       .map((result, i, arr) => {
-        const index = arr.findIndex(r => r.magnetLink === result.magnetLink);
+        if (!result) return;
+        const index = arr.slice(i + 1, arr.length).findIndex(r => r.magnetLink === result.magnetLink);
         if (index === -1) return;
+
+        const oldResult = arr[index];
+        arr[index] = false;
 
         return {
           name: result.name,
           date: result.date,
           magnetLink: result.magnetLink,
-          size: Math.max(result.size, arr[index].size),
-          seed: Math.max(result.seed, arr[index].seed),
-          leech: Math.max(result.leech, arr[index].leech),
+          size: Math.max(result.size, oldResult.size),
+          seed: Math.max(result.seed, oldResult.seed),
+          leech: Math.max(result.leech, oldResult.leech),
         };
       })
       .filter(result => result);
